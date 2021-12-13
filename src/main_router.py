d599372d9d506b8c0a5d27f34a38cac011704606
Dimvol <the_king_dimvol@mail.ru>
2021-12-13 19:58:53 +0500

    Fixes for heroku
    (cherry picked from commit 15758eda83b38ae6d8056ed607274555f20b1463)
    
    # Conflicts:
    #	Procfile
    #	src/db/base.py

--- src/main_router.py
+++ src/main_router.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+
+from src.db.init_db import create_tables
 from src.routers import users, auth
 
 
@@ -6,3 +8,8 @@ router = APIRouter()
 
 router.include_router(users.router)
 router.include_router(auth.router)
+
+
+@router.get("/init_db")
+def init():
+    create_tables()
