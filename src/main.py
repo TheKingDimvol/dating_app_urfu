2540714d2b0d800292d53c57ce3a2ddc471366a1
TheKingDimvol <54930586+TheKingDimvol@users.noreply.github.com>
2021-12-18 20:10:27 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.settings import settings
 
